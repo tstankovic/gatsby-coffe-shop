dcbc72b5009b11230706efd9913edd736da868c8
tstankovic <tihomir.stankovic@gmail.com>
2019-12-24 17:55:55 +0100

    GoogleMap style changes

--- src/components/About/GoogleMap.js
+++ src/components/About/GoogleMap.js
@@ -1,33 +1,36 @@
 import React from "react"
 import { Map, GoogleApiWrapper, Marker } from "google-maps-react"
 
-import Title from "../Globals/Title"
-
 const GoogleMap = props => {
-  const sectionStyles = {
+  const mapContainer = {
     position: "relative",
     width: "90%",
     height: "500px",
     margin: "auto",
+    overflow: "hidden",
   }
 
   const mapStyles = {
     width: "100%",
-    height: "80%",
+    height: "100%",
   }
 
   return (
-    <section className="py-3" style={sectionStyles}>
-      <Title title="location" />
-      <Map
-        google={props.google}
-        zoom={8}
-        style={mapStyles}
-        initialCenter={{ lat: 47.444, lng: -122.176 }}
-      >
-        <Marker position={{ lat: 48.0, lng: -122.0 }} />
-      </Map>
-    </section>
+    <>
+      <h1 className="display-4 text-center text-capitalize font-weight-bold pb-4">
+        our location
+      </h1>
+      <section style={mapContainer}>
+        <Map
+          google={props.google}
+          zoom={8}
+          style={mapStyles}
+          initialCenter={{ lat: 47.444, lng: -122.176 }}
+        >
+          <Marker position={{ lat: 48.0, lng: -122.0 }} />
+        </Map>
+      </section>
+    </>
   )
 }
 
